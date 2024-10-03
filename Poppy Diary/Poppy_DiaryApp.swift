2ad95baa88eac8abcd54f5b291fdf1491da4e50c
anisa <74878427+anihab@users.noreply.github.com>
2024-10-03 14:25:20 -0600

    Initial Commit

---
+++ Poppy Diary/Poppy_DiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poppy_DiaryApp.swift
+//  Poppy Diary
+//
+//  Created by Anisa Habib on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Poppy_DiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Poppy DiaryTests/Poppy_DiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Poppy_DiaryTests.swift
+//  Poppy DiaryTests
+//
+//  Created by Anisa Habib on 10/3/24.
+//
+
+import Testing
+@testable import Poppy_Diary
+
+struct Poppy_DiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
